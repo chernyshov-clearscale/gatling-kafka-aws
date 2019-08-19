f57f4b7ac4d8b723326cc1c01b484aa3ece573b3
Ivan Zolotov <izolotov@gryddinamics.com>
2019-08-19 15:38:24 +0300

    fix

--- bin/run.sh
+++ bin/run.sh
@@ -43,6 +43,12 @@ fi
 ## Clean reports
 rm -rf target/gatling/*
 
+echo $AWS_CONTAINER_CREDENTIALS_RELATIVE_URI
+CREDS_JSON=curl 169.254.170.2$AWS_CONTAINER_CREDENTIALS_RELATIVE_URI
+export AWS_ACCESS_KEY_ID=echo $CREDS_JSON | jq -r .AccessKeyId
+export AWS_SECRET_ACCESS_KEY=echo $CREDS_JSON | jq -r .SecretAccessKey
+export AWS_SESSION_TOKEN=echo $CREDS_JSON | jq -r .Token
+
 # Running performance test
 #mvn gatling:test -Dgatling.simulationClass=nl.codecontrol.gatling.simulations.BasicSimulation
 mvn gatling:test -Dgatling.simulationClass=nl.codecontrol.gatling.simulations.KafkaSimulation
